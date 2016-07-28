e79c6e1c292dddd619d240ee8026b90887e09171
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-07-28 07:29:05 -0600

    run an experiment of my own

---
+++ luigi_experiments/test2.py
@@ -0,0 +1,28 @@
+import random
+import luigi
+
+
+class FirstStep(luigi.Task):
+    max_value = luigi.IntParameter()
+    def requires(self):
+        return []
+    def output(self):
+        return luigi.LocalTarget('number_less_than_{}.txt'.format(self.max_value))
+    def run(self):
+        with self.output().open('w') as out_file:
+            output = int(round(random.random() * self.max_value))
+            out_file.write('{}'.format(output))
+
+class SecondStep(luigi.Task):
+    def requires(self):
+        return [FirstStep(max_value=5)]
+    def output(self):
+        return luigi.LocalTarget('random_number_squared.txt')
+    def run(self):
+        with self.output().open('w') as out_file, self.input()[0].open() as in_file:
+            value = int(in_file.read())
+            out_file.write(str(value*value))
+
+if __name__ == '__main__':
+    luigi.run()
+
