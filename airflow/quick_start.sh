2ba176c6ca786d6ed26d83f235e6fa021d218611
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-08-17 07:31:14 -0600

    add shell script for quickstart

---
+++ airflow/quick_start.sh
@@ -0,0 +1,15 @@
+# this code from https://pythonhosted.org/airflow/start.html
+
+# airflow needs a home, ~/airflow is the default,
+# but you can lay foundation somewhere else if you prefer
+# (optional)
+export AIRFLOW_HOME=~/airflow
+
+# install from pypi using pip
+pip install airflow
+
+# initialize the database
+airflow initdb
+
+# start the web server, default port is 8080
+airflow webserver -p 8080
